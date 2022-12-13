071f717c68ef7ca2ba121671668a7a3d11008047
root <spklpool@gmail.com>
2022-12-13 02:04:13 +0000

    making monitoring work on gunicorn

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ from watchdog.events import FileSystemEventHandler
 
 class DataFileChangedHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        print(f'event type: {event.event_type}  path : {event.src_path}')
-        try:
-            pool_json = json.load(open(event.src_path))
-            map_of_pool_jsons[pool_json['ticker']] = pool_json
-        except Exception as e:
-            logging.error(f'{e}. Continuing execution...')
+        if event.src_path.endswith('.json'):
+            time.sleep(0.1)
+            logging.info(f'event type: {event.event_type}  path : {event.src_path}')
+            try:
+                pool_json = json.load(open(event.src_path))
+                map_of_pool_jsons[pool_json['ticker']] = pool_json
+            except Exception as e:
+                logging.error(f'{e}. Continuing execution...')
 
 
 logging.basicConfig(level=logging.INFO, force=True)
@@ -25,24 +27,26 @@ app = Flask(__name__)
 
 map_of_pool_jsons = ThreadSafeDictOfPoolJson()
 
-directory = 'data'
+directory = '/root/git/cardaviz/data/'
+logging.info('starting initial load of datadirectory [' + directory + '] for changes')
 all_files = os.listdir(directory)
 for filename in all_files:
     pool_file = os.path.join(directory, filename)
     if os.path.isfile(pool_file):
-        print(filename)
         pool_json = json.load(open(pool_file))
         map_of_pool_jsons[pool_json['ticker']] = pool_json
 
+logging.info('starting to monitor directory [' + directory + '] for changes')
 event_handler = DataFileChangedHandler()
 observer = Observer()
-observer.schedule(event_handler, path='data/', recursive=False)
+observer.schedule(event_handler, path=directory, recursive=False)
 observer.start()
+logging.info('directory monitoring started')
 
 original_handler = signal.getsignal(signal.SIGINT)
 
 def sigint_handler(signum, frame):
-    print("interrupt received")
+    logging.info("interrupt received")
     original_handler(signum, frame)
 
 try:

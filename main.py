7fd928c1add3d8d9119281cf24bbd38134bfcb2a
spklpool <spklpool@gmail.com>
2022-12-12 20:17:26 -0500

    auto reload files into memory on change

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
+import os
 import time
-
-
+import signal
+import logging
+import simplejson as json
 from flask import Flask, render_template
+from thread_safe_objects import ThreadSafeDictOfPoolJson
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
+
+class DataFileChangedHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        print(f'event type: {event.event_type}  path : {event.src_path}')
+        try:
+            pool_json = json.load(open(event.src_path))
+            map_of_pool_jsons[pool_json['ticker']] = pool_json
+        except Exception as e:
+            logging.error(f'{e}. Continuing execution...')
+
+
+logging.basicConfig(level=logging.INFO, force=True)
 
 app = Flask(__name__)
+
+map_of_pool_jsons = ThreadSafeDictOfPoolJson()
+
+directory = 'data'
+all_files = os.listdir(directory)
+for filename in all_files:
+    pool_file = os.path.join(directory, filename)
+    if os.path.isfile(pool_file):
+        print(filename)
+        pool_json = json.load(open(pool_file))
+        map_of_pool_jsons[pool_json['ticker']] = pool_json
+
+event_handler = DataFileChangedHandler()
+observer = Observer()
+observer.schedule(event_handler, path='data/', recursive=False)
+observer.start()
+
+original_handler = signal.getsignal(signal.SIGINT)
+
+def sigint_handler(signum, frame):
+    print("interrupt received")
+    original_handler(signum, frame)
+
+try:
+    signal.signal(signal.SIGINT, sigint_handler)
+except ValueError as e:
+    logging.error(f'{e}. Continuing execution...')
 
 
 #TODO: add a landing page here
@@ -21,7 +65,7 @@ def get_pool(pool_ticker):
 @app.route("/pools/<pool_ticker>/epochs")
 def get_pool_epochs(pool_ticker):
     start = time.time()
-    loaded_file = open("data/" + pool_ticker.upper() + ".json", "r").read()
+    loaded_file = map_of_pool_jsons[pool_ticker.upper()]
     print(f'Time: {time.time() - start}')
     return loaded_file
 
---
+++ thread_safe_objects.py
@@ -0,0 +1,25 @@
+from threading import Lock
+
+class ThreadSafeDictOfPoolJson():
+    def __init__(self):
+        self.inner_lock = Lock()
+        with self.inner_lock:
+            self.inner_dict = {}
+
+
+    def update(self, ticker, json):
+        with self.inner_lock:
+            self.inner_dict[ticker] = json
+
+
+    def __getitem__(self, ticker):
+        with self.inner_lock:
+            return self.inner_dict[ticker]
+
+    def __setitem__(self, ticker, json):
+        with self.inner_lock:
+            self.inner_dict[ticker] = json
+
+    def __repr__(self):
+        return 'ThreadSafeDictOfPoolJson({})'.format(self.inner_dict)
+

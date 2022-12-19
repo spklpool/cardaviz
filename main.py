2fb844b6300d6dde255fb47d96269531604ab925
spklpool <spklpool@gmail.com>
2022-12-18 19:51:34 -0500

    templated underappreciated performers

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ app = Flask(__name__)
 
 map_of_pool_jsons = ThreadSafeDictOfPoolJson()
 
-#directory = 'data/'
-directory = '/var/www/html/data/'
+directory = 'data/'
+#directory = '/var/www/html/data/'
 logging.info('starting initial load of datadirectory [' + directory + '] for changes')
 all_files = os.listdir(directory)
 for filename in all_files:
@@ -69,6 +69,52 @@ def get_landing_page():
 @app.route("/pools/<pool_ticker>")
 def get_pool(pool_ticker):
     return render_template('perfchart.html', pool_ticker=pool_ticker.upper())
+
+@app.route("/performers")
+def get_underappreciated_performers():
+    pools = []
+    tickers_json = json.load(open('static/tickers.json'))
+    for (pool_ticker, pool_id) in tickers_json.items():
+        pool = {}
+        pool['cumulative_diff'] = 0
+        pool['cumulative_epochs'] = 0
+        pool['cumulative_blocks'] = 0
+        pool['live_stake'] = 0
+        pool['ticker'] = pool_ticker
+        pool_file = directory + "/" + pool_ticker.upper() + ".json"
+        try:
+            pool_json = json.load(open(pool_file))
+            for epoch in pool_json['epochs']:
+                epoch_diff = epoch['actual'] - epoch['expected']
+                pool['cumulative_diff'] += epoch_diff
+                pool['cumulative_epochs'] += 1
+                pool['cumulative_blocks'] += epoch['actual']
+                pool['live_stake'] = epoch['pool_stake']
+                pool['total_stake'] = epoch['total_stake']
+            if pool['live_stake'] > 0:
+                pool['block_performance'] = pool['cumulative_diff'] * pool['cumulative_blocks']
+                pool['epochs_performance'] = pool['cumulative_diff'] * pool['cumulative_epochs']
+                pool['total_performance'] = pool['cumulative_diff'] * pool['cumulative_blocks'] * pool['cumulative_epochs']
+                pool['underappreciated'] = pool['cumulative_diff'] * pool['cumulative_epochs'] / (pool['live_stake'] / pool['total_stake'])
+                pools.append(pool)
+        except (Exception) as error:
+            print("Unable to load " + pool_ticker)
+            print(repr(error))
+
+    performers = []
+    sorted_by_underappreciated_performance = sorted(pools, key=lambda d: d['underappreciated'], reverse=True)
+    rank = 0
+    for pool in sorted_by_underappreciated_performance:
+        if rank < 10:
+            if pool['live_stake'] > (10000 * 1000000):
+                if pool['cumulative_blocks'] > 5:
+                    current_performer = {}
+                    rank += 1
+                    current_performer['rank'] = rank
+                    current_performer['ticker'] = pool['ticker']
+                    performers.append(current_performer)
+
+    return render_template('underappreciated_performers.html', pools=performers)
 
 @app.route("/data/<pool_ticker>.json")
 def get_pool_epochs(pool_ticker):

796ffb95391e8bd4ac75223599460c424297287c
spklpool <spklpool@gmail.com>
2023-01-30 14:16:40 -0500

    removing database to simplify webapp

--- src/main.rs
+++ src/main.rs
@@ -22,15 +22,6 @@ fn hello(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
-#[get("/latestepoch")]
-async fn latestepoch(mut db: Connection<CExplorer>) -> String {
-    let epochrow = sqlx::query("SELECT MAX(NO) as latest_epoch FROM epoch;")
-        .fetch_one(&mut *db).await
-        .expect("failed getting the latest epoch from the database");
-    let epoch: i32 = epochrow.get(0);
-    format!("latest epoch: {}", epoch)
-}
-
 #[get("/pools/<ticker>")]
 fn templated(ticker: &str) -> Template {
     Template::render("perfchart", context! { ticker: ticker })
@@ -45,7 +36,6 @@ fn index() -> Template {
 fn rocket() -> _ {
     rocket::build()
         .attach(Template::fairing())
-        .attach(CExplorer::init())
         .mount("/", routes![index, hello, templated, latestepoch])
         .mount("/static", FileServer::from("./static"))
         .mount("/data", FileServer::from("./data"))

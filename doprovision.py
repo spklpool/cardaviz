16a26206767a9ee34e5636c3708977048c7497be
spklpool <spklpool@gmail.com>
2024-04-06 15:41:58 +0000

    fixing provisioning

--- doprovision.py
+++ doprovision.py
@@ -20,9 +20,9 @@ git clone https://github.com/spklpool/cardaviz.git
 cd /cardaviz
 python3 -m venv /cardaviz/venv
 /cardaviz/venv/bin/pip3 install -r requirements.txt
-mkdir /var/www/html/data
-cp -r /cardaviz/data/* /var/www/html/data/
-chown -R root:www-data /var/www/html/data/
+mkdir /var/www/html/mainnet_data
+cp -r /cardaviz/data/* /var/www/html/mainnet_data/
+chown -R root:www-data /var/www/html/mainnet_data/
 cp /cardaviz/etc/cardaviz_backend.service /etc/systemd/system/cardaviz_backend.service
 systemctl start cardaviz_backend.service
 systemctl enable cardaviz_backend.service
@@ -37,7 +37,7 @@ cp /cardaviz/etc/ssl-dhparams.pem /etc/letsencrypt/ssl-dhparams.pem
 """,
                                 region='nyc3',
                                 ssh_keys=['f9:c9:f9:d0:15:8a:5d:68:02:f5:9d:26:66:ff:a9:7f'],
-                                image='ubuntu-22-10-x64',
+                                image='ubuntu-22-04-x64',
                                 size_slug='s-1vcpu-1gb-intel',
                                 backups=False)
 
